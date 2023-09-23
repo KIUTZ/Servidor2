bcff8adc93c551efebb672891ea3478334fc7b91
Zaida <zaidapastor2002@gmail.com>
2023-09-23 13:41:52 +0200

    Esqueleto login hecho

--- index.php
+++ index.php
@@ -1,33 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Proyecto 3A Zpasgon</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/bootstrap.min.css" />
-</head>
-
-<body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <ul class="nav navbar-nav">
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Inicio</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Nosotros</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Nav 2</a>
-                </li>
-
-            </ul>
-        </nav>
-        <div class="container">
-            <div class="row">
-
+<?php include("template/cabecera.php"); ?>
                 <div class="jumbotron">
                     <h1 class="display-3"> Jumbo heading </h1>
                     <p class="lead"> Jumbo helper text </p>
@@ -36,11 +7,5 @@
                     <p class="lead">
                         <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button"> Jumbo action name</a>
                     </p>
-
                 </div>
-
-            </div>
-        </div>
-
-</body>
-</html>
+<?php include("template/pie.php"); ?>
