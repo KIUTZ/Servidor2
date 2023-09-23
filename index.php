f968f9547ca3f3d230ad2c1540e4108bafb96931
Zaida <zaidapastor2002@gmail.com>
2023-09-23 12:29:02 +0200

    Cabecera creada, fuente y primeros botones

--- index.php
+++ index.php
@@ -6,17 +6,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/bootstrap.min.css" />
 </head>
+
 <body>
-        Sitio Web
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary-subtle">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <ul class="nav navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Nav 1 <span class="sr-only">(current)</span></a>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inicio</a>
                 </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nosotros</a>
+                </li>
+
                 <li class="nav-item">
                     <a class="nav-link" href="#">Nav 2</a>
                 </li>
+
             </ul>
         </nav>
+        <div class="container">
+            <div class="row">
+
+                <div class="jumbotron">
+                    <h1 class="display-3"> Jumbo heading </h1>
+                    <p class="lead"> Jumbo helper text </p>
+                    <hr class="my-2">
+                    <p>More info</p>
+                    <p class="lead">
+                        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button"> Jumbo action name</a>
+                    </p>
+
+                </div>
+
+            </div>
+        </div>
+
 </body>
 </html>
