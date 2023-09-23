a05f6313186e1b096175037b545959b996ceef0b
Zaida <zaidapastor2002@gmail.com>
2023-09-23 12:07:13 +0200

    Primera estructura de la web creada

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Proyecto 3A Zpasgon</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+</head>
+<body>
+        Sitio Web
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary-subtle">
+            <ul class="nav navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Nav 1 <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nav 2</a>
+                </li>
+            </ul>
+        </nav>
+</body>
+</html>
